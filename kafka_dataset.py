55c867476be114525ca3ce3190c07834ae4de82f
Benjamin Riviere <benjamin.riviere@linkfluence.com>
2020-10-15 22:13:43 +0200

    Merge the 2 files.

--- kafka_dataset.py
+++ kafka_dataset.py
@@ -1,3 +1,4 @@
+import itertools as it
 import logging
 import os
 import signal
@@ -5,10 +6,11 @@ import sys
 from kafka import KafkaConsumer
 from kafka.errors import CommitFailedError
 from torch.utils.data import get_worker_info
-from torch.utils.data import IterableDataset
+from torch.utils.data import DataLoader, IterableDataset
 import torch.multiprocessing as mp
 
 
+__all__ = ["KafkaDataset", "auto_commit"]
 _logger = logging.getLogger(__name__)
 
 
@@ -180,3 +182,67 @@ class KafkaDataset(IterableDataset):
         dataset._consumer = None
 
         return dataset
+
+
+class _AutoCommitterIterator:
+    """Define a wrapper on top of a PyTorch DataLoader to auto-magically
+    commits data from a KafkaDataset.
+
+    Parameters
+    ----------
+    dataloader : torch.utils.data.DataLoader
+        The dataloader to commit the dataset/workers from.
+        It must uses a KafkaDataset.
+    """
+    def __init__(self, dataloader: DataLoader):
+        if not isinstance(dataloader, DataLoader):
+            raise TypeError("A DataLoader must be provided.")
+
+        if not issubclass(dataloader.dataset.__class__, KafkaDataset):
+            raise TypeError("The DataLoader must iterate over a KafkaDataset.")
+
+        self.dataloader = dataloader
+        self._dataset_class = dataloader.dataset.__class__
+
+    def __iter__(self):
+        # Define how to iter on the data
+        # The way we process depends if we are using singleprocessing or
+        # multiprocessing.
+        if self.dataloader.num_workers == 0:
+            # For singleprocessing, it's easy, we just need to commit the
+            # dataset every time.
+            for batch in self.dataloader:
+                yield batch
+
+                self.dataloader.dataset.commit()
+        else:
+            # Workers are actually created when iterated.
+            # We need access to the iterator because it saves references to
+            # workers and we need them to send commit signals.
+            batches = iter(self.dataloader)
+
+            # Cycling through the workers as the DataLoader
+            workers = it.cycle(batches._workers)
+
+            for w, batch in zip(workers, batches):
+                yield batch
+
+                self._dataset_class.commit_worker(w)
+
+
+def auto_commit(dataloader: DataLoader):
+    """Auto-magically commits data from a KafkaDataset.
+
+    Parameters
+    ----------
+    dataloader : torch.utils.data.DataLoader
+        The dataloader to commit the dataset/workers from.
+        It must uses a KafkaDataset.
+
+    Returns
+    -------
+    iterator
+        An iterator over the DataLoader that will commit the batches as they
+        come.
+    """
+    return _AutoCommitterIterator(dataloader)
